2846f9004769544295e8ac90806f56f99439ad7a
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-07-27 10:17:40 +0100

    added reusable/common widgets

---
+++ lib/app/vendors/vendors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Vendors extends StatefulWidget {
+  const Vendors({super.key});
+
+  @override
+  State<Vendors> createState() => _VendorsState();
+}
+
+class _VendorsState extends State<Vendors> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
