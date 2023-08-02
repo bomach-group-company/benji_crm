1ec2a4046dd3a8416dea41612549c923df876113
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-08-02 16:39:59 +0100

    created the skeleton for the dashboard page

---
+++ lib/src/skeletons/dashboard_page_skeleton.dart
@@ -0,0 +1,62 @@
+import 'package:benji_aggregator/src/skeletons/dashboard_orders_list_skeleton.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../../theme/colors.dart';
+import '../providers/constants.dart';
+import 'page_skeleton.dart';
+
+class DashboardPageSkeleton extends StatelessWidget {
+  const DashboardPageSkeleton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double mediaWidth = MediaQuery.of(context).size.width;
+    return ListView(
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.all(kDefaultPadding),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: List.generate(
+            2,
+            (index) => Shimmer.fromColors(
+              highlightColor: kBlackColor.withOpacity(0.02),
+              baseColor: kBlackColor.withOpacity(0.8),
+              direction: ShimmerDirection.ltr,
+              child: const PageSkeleton(
+                width: 160,
+                height: 140,
+              ),
+            ),
+            growable: true,
+          ),
+        ),
+        kSizedBox,
+        SizedBox(
+          height: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(
+              2,
+              (index) => Shimmer.fromColors(
+                highlightColor: kBlackColor.withOpacity(0.02),
+                baseColor: kBlackColor.withOpacity(0.8),
+                direction: ShimmerDirection.ltr,
+                child: PageSkeleton(
+                  width: mediaWidth,
+                  height: 140,
+                ),
+              ),
+              growable: true,
+            ),
+          ),
+        ),
+        kSizedBox,
+        const DashboardOrdersListSkeleton(),
+      ],
+    );
+  }
+}
