e5f2d2ce759c3c8c7191b24c096ea64c8fe78d27
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-08-02 15:29:04 +0100

    made the listview.separated reusable

--- lib/src/common_widgets/all_vendors_page_skeleton.dart
+++ lib/src/common_widgets/all_vendors_page_skeleton.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
+import '../../theme/colors.dart';
 import '../providers/constants.dart';
+import 'all_vendors_list_skeleton.dart';
 import 'page_skeleton.dart';
 
 class AllVendorsPageSkeleton extends StatelessWidget {
@@ -22,52 +25,19 @@ class AllVendorsPageSkeleton extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: List.generate(
                 2,
-                (index) => const PageSkeleton(height: 35, width: 130),
+                (index) => Shimmer.fromColors(
+                  highlightColor: kBlackColor.withOpacity(0.02),
+                  baseColor: kBlackColor.withOpacity(0.8),
+                  direction: ShimmerDirection.ltr,
+                  child: const PageSkeleton(height: 35, width: 130),
+                ),
                 growable: true,
               ),
             ),
           ),
         ),
         kSizedBox,
-        ListView.separated(
-          separatorBuilder: (context, index) =>
-              const SizedBox(height: kDefaultPadding),
-          itemCount: 30,
-          addAutomaticKeepAlives: true,
-          physics: const BouncingScrollPhysics(),
-          shrinkWrap: true,
-          itemBuilder: (context, index) => const Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              PageSkeleton(height: 120, width: 130),
-              kWidthSizedBox,
-              Column(
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: PageSkeleton(height: 20, width: 200),
-                  ),
-                  kSizedBox,
-                  SizedBox(
-                    width: 200,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        PageSkeleton(height: 15, width: 60),
-                        kHalfWidthSizedBox,
-                        PageSkeleton(height: 15, width: 60),
-                      ],
-                    ),
-                  ),
-                  kHalfSizedBox,
-                  PageSkeleton(height: 15, width: 200),
-                  kSizedBox,
-                  PageSkeleton(height: 15, width: 200),
-                ],
-              ),
-            ],
-          ),
-        )
+        AllVendorsListSkeleton(),
       ],
     );
   }
