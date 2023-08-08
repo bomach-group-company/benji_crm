6f1a72c0df91b9bb093db6ec64c7da82be296047
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-08-08 14:27:24 +0100

    added a completed orders page

---
+++ lib/app/others/my_orders/completed_orders.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompletedOrders extends StatefulWidget {
+  const CompletedOrders({super.key});
+
+  @override
+  State<CompletedOrders> createState() => _CompletedOrdersState();
+}
+
+class _CompletedOrdersState extends State<CompletedOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
