f0ec8805f98406615b33fecdf51c734499a056e0
egideons <egideonchuks@gmail.com>
2023-10-19 17:16:03 +0100

    refactored code

--- lib/app/profile/profile.dart
+++ lib/app/profile/profile.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:benji_aggregator/app/others/withdrawal/withdraw_history.dart';
+import 'package:benji_aggregator/app/others/withdrawal/withdrawal_history.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
