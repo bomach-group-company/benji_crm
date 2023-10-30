a34dae3cfc8ed377cfe301ba06e9567c8e4de21a
egideons <egideonchuks@gmail.com>
2023-10-30 12:15:41 +0100

    made a few changes

--- lib/app/vendors/business_category_modal.dart
+++ lib/app/vendors/business_category_modal.dart
@@ -1,55 +1,67 @@
+import 'package:benji_aggregator/src/components/my_elevatedButton.dart';
 import 'package:benji_aggregator/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../model/business_type_model.dart';
+import '../../src/providers/constants.dart';
 
 Future shopTypeModal(BuildContext context, List<BusinessType> type) async {
+  final scrollController = ScrollController();
   var media = MediaQuery.of(context).size;
   return showModalBottomSheet(
       context: context,
-      // isScrollControlled: true,
-
+      isScrollControlled: true,
+      anchorPoint: const Offset(0, 10),
+      barrierColor: kBlackColor.withOpacity(0.8),
       backgroundColor: kPrimaryColor,
+      showDragHandle: true,
+      isDismissible: true,
+      elevation: 20,
+      enableDrag: true,
+      constraints: BoxConstraints(maxHeight: media.height),
       shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
-      builder: (context) => Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-            child: SizedBox(
-              height: media.height * 0.4,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, top: 10),
+      builder: (context) => Scrollbar(
+            controller: scrollController,
+            child: SingleChildScrollView(
+              controller: scrollController,
+              physics: const BouncingScrollPhysics(),
+              padding: const EdgeInsets.all(kDefaultPadding),
+              child: SizedBox(
+                width: media.width,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  // mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    const Text(
+                      "Business Types",
+                      textAlign: TextAlign.left,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                    ),
                     Column(
                       children: [
-                        const SizedBox(height: 20),
-                        const Text(
-                          "Business Type",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(height: 20),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: type
                               .map(
                                 (e) => InkWell(
                                   onTap: () async {
-                                    Navigator.pop(context, e);
+                                    Get.back(result: e);
                                   },
                                   child: Padding(
                                     padding: const EdgeInsets.symmetric(
                                         vertical: 10),
-                                    child: Text(e.name ?? ""),
+                                    child: Text(e.name),
                                   ),
                                 ),
                               )
                               .toList(),
                         ),
                         const SizedBox(height: 20),
+                        MyElevatedButton(
+                            title: "Get Business Types", onPressed: () {})
                       ],
                     ),
                   ],
@@ -57,11 +69,4 @@ Future shopTypeModal(BuildContext context, List<BusinessType> type) async {
               ),
             ),
           ));
-}
-
-class ItemWeight {
-  String item;
-  bool selected;
-  int id;
-  ItemWeight({required this.item, required this.selected, required this.id});
 }
