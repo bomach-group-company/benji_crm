2f1fc9b13f9aded1a54c73ec1ec20346d65cfc88
egideons <egideonchuks@gmail.com>
2024-01-18 10:40:12 +0100

    refactored code

--- lib/app/others/notifications.dart
+++ lib/app/others/notifications.dart
@@ -3,14 +3,18 @@
 import 'package:benji_aggregator/controller/notification_controller.dart';
 import 'package:benji_aggregator/controller/operation.dart';
 import 'package:benji_aggregator/src/components/card/empty.dart';
+import 'package:benji_aggregator/src/components/image/my_image.dart';
 import 'package:benji_aggregator/src/skeletons/notifications_page_skeleton.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 
+import '../../controller/user_controller.dart';
 import '../../model/notificatin_model.dart';
 import '../../src/components/appbar/my_appbar.dart';
 import '../../src/components/section/my_liquid_refresh.dart';
 import '../../src/providers/constants.dart';
+import '../../src/responsive/responsive_constant.dart';
 import '../../theme/colors.dart';
 
 class Notifications extends StatefulWidget {
@@ -25,8 +29,8 @@ class _NotificationsState extends State<Notifications> {
   @override
   void initState() {
     super.initState();
-
     _loadingScreen = true;
+    scrollController.addListener(_scrollListener);
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       NotificationController.instance.runTask();
     });
@@ -35,9 +39,10 @@ class _NotificationsState extends State<Notifications> {
 
   //=================================== ALL VARIABLES =====================================\\
   late bool _loadingScreen;
+  bool _isScrollToTopBtnVisible = false;
 
   //============================================== CONTROLLERS =================================================\\
-  final ScrollController scrollController = ScrollController();
+  final scrollController = ScrollController();
 
   //=================================== FUNCTIONS =====================================\\
 
@@ -55,9 +60,26 @@ class _NotificationsState extends State<Notifications> {
     });
   }
 
+  //============================= Scroll to Top ======================================//
+  void scrollToTop() {
+    scrollController.animateTo(0,
+        duration: const Duration(seconds: 1), curve: Curves.fastOutSlowIn);
+  }
+
+  void _scrollListener() {
+    //========= Show action button ========//
+    if (scrollController.position.pixels >= 100) {
+      setState(() => _isScrollToTopBtnVisible = true);
+    }
+    //========= Hide action button ========//
+    else if (scrollController.position.pixels < 100) {
+      setState(() => _isScrollToTopBtnVisible = false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // var media = MediaQuery.of(context).size;
+    var media = MediaQuery.of(context).size;
     return MyLiquidRefresh(
       onRefresh: _handleRefresh,
       child: Scaffold(
@@ -68,6 +90,23 @@ class _NotificationsState extends State<Notifications> {
           elevation: 0,
           actions: const [],
         ),
+        floatingActionButton: _isScrollToTopBtnVisible
+            ? FloatingActionButton(
+                onPressed: scrollToTop,
+                mini: deviceType(media.width) > 2 ? false : true,
+                backgroundColor: kAccentColor,
+                enableFeedback: true,
+                mouseCursor: SystemMouseCursors.click,
+                tooltip: "Scroll to top",
+                hoverColor: kAccentColor,
+                hoverElevation: 50.0,
+                child: FaIcon(
+                  FontAwesomeIcons.chevronUp,
+                  size: 18,
+                  color: kPrimaryColor,
+                ),
+              )
+            : const SizedBox(),
         body: SafeArea(
           child: GetBuilder<NotificationController>(
             builder: (notifications) {
@@ -101,11 +140,18 @@ class _NotificationsState extends State<Notifications> {
                                     minVerticalPadding: kDefaultPadding / 2,
                                     enableFeedback: true,
                                     leading: Container(
-                                      width: 45,
-                                      height: 45,
+                                      width: 50,
+                                      height: 50,
                                       decoration: ShapeDecoration(
                                         color: kPageSkeletonColor,
                                         shape: const OvalBorder(),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(10),
+                                        child: MyImage(
+                                          url: UserController
+                                              .instance.user.value.image,
+                                        ),
                                       ),
                                     ),
                                     title: Text.rich(
@@ -145,8 +191,8 @@ class _NotificationsState extends State<Notifications> {
                                           ? "loading..."
                                           : Operation.convertDate(
                                               notify.created!),
-                                      style: const TextStyle(
-                                        color: Color(0xFF9B9BA5),
+                                      style: TextStyle(
+                                        color: kAccentColor,
                                         fontSize: 10,
                                         fontWeight: FontWeight.w400,
                                       ),
@@ -154,6 +200,7 @@ class _NotificationsState extends State<Notifications> {
                                   );
                                 },
                               ),
+                              kSizedBox,
                             ],
                           ),
                         );
